3d605a9a5c5b71cd12cecd309582499cf13de298
aneesalikehar <134583553+aneesalikehar@users.noreply.github.com>
2023-08-09 09:26:17 -0700

    Add files via upload

---
+++ java script assignment/app.js
@@ -0,0 +1,161 @@
+// .................chapter 1 alert ............
+
+// alert ("Error! Please enter a valid password")
+
+
+// alert ("Welcome to JS land \n Happy coding")
+
+
+// alert ("Welcome to JS land");
+
+// alert ("Happy Coding \n Preventin this page from creating additional dialogue");
+
+// alert ("Hello... I can JS through my browser console")
+
+
+
+// .......................chapter 2 variabler for String...............
+
+
+// var userName = "Anees";
+
+// alert (userName)
+
+// 2. 
+// var myName = "Anees Ali"
+
+// alert(myName)
+
+// 3
+// var message = "Hello World"
+
+// alert(message)
+
+// 4
+// var studentName = "Anees Ali";
+
+// alert(studentName);
+
+// var age = "23 Years old";
+
+// alert(age);
+
+// var study = "Certified Mobile Application Development";
+
+// alert(study);
+
+
+// 5
+// var product = " Pizza \n Pizz \n Piz \n Pi \n P"
+
+// alert (product)
+
+// 6
+// var email = "aneesalikehar@gmail.com"
+
+// alert(email)
+
+// 7
+// var book = "A smarter way to learn JavaScript";
+
+// alert(book)
+
+// 8
+// var text = "Yeah I can write Html content through JS"
+
+// document.write(text)
+
+
+// 9
+// var line = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+// alert(line)
+
+
+// ..............................chapter 3 .........
+
+// 1
+// var age = "I am 15 years old"
+
+// alert(age)
+
+
+// 2
+// var vistedTimes = "You have visited this sites 14 times"
+
+// alert(vistedTimes)
+
+// 3
+// var birthYear = "My birth year is 1999"
+
+// document.write(birthYear);
+
+
+// 4
+// var vistorsName ="John doe"
+// var producttitle = "ordered"
+// var Quentity = "5 t shirts"
+// var storeName = "XYZ Clothing store"
+
+// document.write(vistorsName + " " + producttitle + " " + Quentity + " " + storeName)
+
+// ..............chapter no 4 ........................
+// 1
+// var x = 10;
+// var y = 10;
+// var z = 10;
+
+// console.log(x+y+z)
+
+// 2
+// var heading = 
+
+// document.write(heading)
+
+
+//3
+// var a = "Variable names can only contain and For example $my"
+
+// document.write(a)
+
+
+// ............chapter 5 ............................
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1+num2)
+
+// document.write("sum of 3 and 5 is" +" "+ num3)
+
+
+//2
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1*num2)
+
+// document.write("sum of 3 and 5 is" +" "+ num3)
+
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1-num2)
+
+// document.write("sum of 3 and 5 is" +" "+ num3)
+
+//3
+// var text = "value afte variable is declaraition is undefined"
+
+// var num = 5;
+// var newNum = ++num;
+// console.log = (value1 + newNum);
+
+// // console.log(result);
+
+var num = 5;
+var newNum = ++num;
+
+var num4 = num * newNum;
+console.log(num4)
+
+
+
+
+
